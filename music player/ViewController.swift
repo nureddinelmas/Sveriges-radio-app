d34596c8565ee2896602e32a8bab7e0c92bb582c
nureddinelmas <nureddinelmas34@gmail.com>
2021-12-06 23:30:11 +0100

    Initial Commit

---
+++ music player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  music player
+//
+//  Created by Nureddin Elmas on 2021-12-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
