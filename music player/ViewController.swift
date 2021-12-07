ac7ab9b0bdbc8e2d1e5b89f1bd3bca52809ef126
nureddinelmas <nureddinelmas34@gmail.com>
2021-12-07 10:47:30 +0100

    Sveriges en radio station, simple app, press button for playing and press again for pausing :)

--- music player/ViewController.swift
+++ music player/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBAction func slider(_ sender: UISlider) {
+        player?.volume = sender.value
+    }
+    
+    var player:AVPlayer?
+    var playerItem:AVPlayerItem?
+    var kontrol = true
+    let url1 = "http://sverigesradio.se/topsy/direkt/srapi/207.mp3"
+    let url = URL(string: "http://sverigesradio.se/topsy/direkt/srapi/207.mp3")
+    var audioPlayer = AVAudioPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
+        player = AVPlayer(playerItem: playerItem)
     }
-
+  
+    
+    @IBAction func pressed(_ sender: UIButton) {
+        if kontrol {
+            player?.play()
+            kontrol = false
+            sender.setTitle("Pause", for: .normal)
+           
+        } else{
+            player?.pause()
+            kontrol = true
+            sender.setTitle("Play", for: .normal)
+        }
+        
+    }
 
 }
 
